8191c9b3fc08f4d912d9d63189c0d1ef4d2874ab
reona-fujii <ya5t4yfxwbcshb908r0s@gmail.com>
2021-02-19 09:32:24 +0900

    edit

--- my-app3/src/index.js
+++ my-app3/src/index.js
@@ -4,8 +4,10 @@ import {render} from 'react-dom';
 import tasksReducer from './reducers/tasks';
 import TodoApp from './components/TodoApp';
 import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
-const store=createStore(tasksReducer);
+const store=createStore(reducer, applyMiddleware(logger));
 
 render (
   <Provider store={store}>
